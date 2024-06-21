4640c3961c7b15831903eab46589d71a2b5aa212
Serhii Pylypchuk <pylypchukserhii1991@gmail.com>
2024-06-21 09:44:30 +0400

    [dev] refactoring

--- src/ToDo.jsx
+++ src/ToDo.jsx
@@ -1,19 +1,29 @@
 import { useEffect, useRef } from "react";
-import { ToDo } from "@dhx/trial-todolist";
+import { ToDo, Toolbar } from "@dhx/trial-todolist";
 import "@dhx/trial-todolist/dist/todo.css";
 
-export default function CalendarView(props) {
-  let container = useRef();
+export default function ToDoComponent(props) {
+  let todo_container = useRef();
+  let toolbar_container = useRef();
 
   useEffect(() => {
-    new ToDo(container.current, {
+    const todo = new ToDo(todo_container.current, {
       tasks: props.tasks,
       users: props.users,
       projects: props.projects,
+      // other configuration properties
     });
 
-    return () => (container.current.innerHTML = "");
+    new Toolbar(toolbar_container.current, {
+      api: todo.api,
+      // other configuration properties
+    });
+
+    return () => (todo_container.current.innerHTML = "", toolbar_container.current.innerHTML = "");
   }, []);
 
-  return <div ref={container} style={{ width: "100%", height: "100%" }}></div>;
+  return  <div>
+            <div ref={toolbar_container}></div>
+            <div ref={todo_container} style={{ width: "100%", height: "100%" }}></div>
+          </div>
 }
